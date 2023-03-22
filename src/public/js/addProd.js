341ccf05a1ba5e7d1ad166a42c4aad3cd2071a4e
Fpiatti98 <franco.piatti698@gmail.com>
2023-03-22 20:54:24 -0300

    funcionalidad del boton agregar al carrito

--- src/public/js/addProd.js
+++ src/public/js/addProd.js
@@ -1,4 +1,4 @@
-const addProdToCart = async () => {
+const addProdToCart = (id) => {
 
     /*
     fetch('/js/test.js')
@@ -7,8 +7,20 @@ const addProdToCart = async () => {
     // Evaluate the function and use it
     const myFunction = eval(data);
   })
-  */
+  
   const response = await fetch('/js/test.js');
   const data = await response.text()
   const funcion = await eval(data)
+  */
+
+  fetch(`http://localhost:8080/mongodb/api/carts/641501090cd3fa0a61861a7c/product/${id}`, {
+    method: 'POST',
+    mode: "cors",
+  })
+  .then(result => {
+    if(result.status==200){
+      alert("producto agregado");
+    }
+  });
+  
 };
